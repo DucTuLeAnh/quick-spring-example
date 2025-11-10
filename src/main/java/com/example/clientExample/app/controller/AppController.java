a3bd270da6a1ecf98fb363e8a5cce17ee0d2af4b
Duc Tu Le Anh <duc.tu.le.anh@gmail.com>
2025-11-10 22:35:08 +0100

    chore: experiment with oauth2 authentication

--- src/main/java/com/example/clientExample/app/controller/AppController.java
+++ src/main/java/com/example/clientExample/app/controller/AppController.java
@@ -3,8 +3,14 @@ package com.example.clientExample.app.controller;
 import com.example.clientExample.app.entities.ExampleResponse;
 import com.example.clientExample.app.service.AppService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
+import org.springframework.security.oauth2.server.resource.introspection.OAuth2IntrospectionAuthenticatedPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
 public class AppController {
@@ -18,5 +24,24 @@ public class AppController {
     @GetMapping("/api/data")
     public ExampleResponse getData() {
         return appService.fetchExampleData();
+    }
+
+    @GetMapping("/public")
+    public String publicEndpoint() {
+        return "public";
+    }
+
+    @GetMapping("/private")
+    public String privateEndpoint(Authentication authentication) {
+
+        if(authentication == null){
+            return "Didnt work";
+        }
+        BearerTokenAuthentication auth = (BearerTokenAuthentication) authentication;
+
+        OAuth2IntrospectionAuthenticatedPrincipal principal =
+                (OAuth2IntrospectionAuthenticatedPrincipal) authentication.getPrincipal();
+
+        return "Hello " + auth.getName() + ", principal=" + principal.getName();
     }
 }
--- src/main/java/com/example/clientExample/shared/TestSecurityConfig.java
+++ src/main/java/com/example/clientExample/shared/TestSecurityConfig.java
@@ -1,34 +1,51 @@
 package com.example.clientExample.shared;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @Profile("test")
 public class TestSecurityConfig {
+
+    private final OAuth2ResourceServerProperties properties;
+
+    public TestSecurityConfig(OAuth2ResourceServerProperties properties) {
+        this.properties = properties;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()  // allow everything
+                        .requestMatchers("/public/**", "/", "/view").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .csrf(AbstractHttpConfigurer::disable);
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .opaqueToken(token -> {
+                            token.introspectionUri(properties.getOpaquetoken().getIntrospectionUri());
+                            token.introspectionClientCredentials(
+                                    properties.getOpaquetoken().getClientId(),
+                                    properties.getOpaquetoken().getClientSecret()
+                            );
+                        })
+                )
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                );
 
         return http.build();
-    }
-
-    // Optional: provide a mock Authentication for testing
-    @Bean
-    @Profile("test")
-    public AuthenticationManager authenticationManager() {
-        return authentication -> {
-            authentication.setAuthenticated(true);
-            return authentication;
-        };
     }
 }
